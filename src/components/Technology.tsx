483f80d7caf0fba224e7353c6bf9008c3786f27f
domi <dominikpil123@gmail.com>
2022-08-27 18:52:54 +0200

    Finished my-works page. 🐒

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,8 +1,18 @@
-import { tech } from "@/data/works";
-import { FC } from "react";
+import { tech } from '@/data/works';
+import Image from 'next/image';
+import { FC } from 'react';
 
-const Technology:FC<{ tech: tech }> = ({ tech }) => {
-	return (<>{tech.name}</>);
-}
- 
+const Technology: FC<{ tech: tech }> = ({ tech }) => {
+	return (
+		<div className='relative aspect-square w-10 mr-5'>
+			<div className='peer'>
+				<Image src={tech.icon} layout='fill'></Image>
+			</div>
+			<span className='absolute left-[125%] top-1/2 -translate-y-1/2 scale-0 peer-hover:scale-100 transition-all z-10 bg-sky-200 px-2 py-1 rounded-lg font-bold'>
+				{tech.name}
+			</span>
+		</div>
+	);
+};
+
 export default Technology;
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -5,7 +5,7 @@ import Technology from './Technology';
 
 const Work: FC<{ work: work }> = ({ work }) => {
 	return (
-		<div className='w-auto aspect-square bg-red-400 m-5 rounded-2xl overflow-hidden drop-shadow-2xl'>
+		<div className='w-auto aspect-square m-5 rounded-2xl overflow-hidden drop-shadow-2xl'>
 			<div className='relative h-36'>
 				<Image
 					src={work.photo}
@@ -16,7 +16,7 @@ const Work: FC<{ work: work }> = ({ work }) => {
 			<div className='bg-gray-100 w-full h-full drop-shadow-sm rounded-t-md shadow-[0_0_41px_rgba(66,68,90,1)] -translate-y-4 p-5 '>
 				<h1 className='font-bold text-center'>{work.title}</h1>
 				<p className='bg-sky-100 p-1 rounded-md drop-shadow'>{work.description}</p>
-			<div>
+			<div className='flex flex-row my-4'>
 				{work.technologies.map(e => <Technology key={e.name} tech={e}></Technology>)}
 			</div>
 			</div>
