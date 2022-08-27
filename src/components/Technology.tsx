527c71d18261b42b429aea8399f0d3e1d35483e9
domi <dominikpil123@gmail.com>
2022-08-27 20:59:32 +0200

    Added night mode to works section. 🐒

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -8,7 +8,7 @@ const Technology: FC<{ tech: tech }> = ({ tech }) => {
 			<div className='peer'>
 				<Image src={tech.icon} layout='fill'></Image>
 			</div>
-			<span className='absolute left-[125%] top-1/2 -translate-y-1/2 scale-0 peer-hover:scale-100 transition-all z-10 bg-sky-200 px-2 py-1 rounded-lg font-bold'>
+			<span className='absolute left-[125%] top-1/2 -translate-y-1/2 scale-0 peer-hover:scale-100 transition-all z-10 bg-stone-800 px-2 py-1 rounded-lg font-bold'>
 				{tech.name}
 			</span>
 		</div>
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -13,9 +13,9 @@ const Work: FC<{ work: work }> = ({ work }) => {
 					objectFit='cover'
 				></Image>
 			</div>
-			<div className='bg-gray-100 w-full h-full drop-shadow-sm rounded-t-md shadow-[0_0_41px_rgba(66,68,90,1)] -translate-y-4 p-5 '>
+			<div className='bg-stone-700 w-full h-full drop-shadow-sm rounded-t-md shadow-[0_0_41px_rgba(66,68,90,1)] -translate-y-4 p-5 '>
 				<h1 className='font-bold text-center'>{work.title}</h1>
-				<p className='bg-sky-100 p-1 rounded-md drop-shadow'>{work.description}</p>
+				<p className='bg-stone-800 p-1 rounded-md drop-shadow'>{work.description}</p>
 			<div className='flex flex-row my-4'>
 				{work.technologies.map(e => <Technology key={e.name} tech={e}></Technology>)}
 			</div>
